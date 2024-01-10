879d467211b32085417f094501f463b5af447975
James Grieve <jgrieve@ualberta.ca>
2024-01-09 20:28:55 -0700

    Basic class;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,6 +34,74 @@ async function main() {
     // If you need to include functions in your JSON, you can use a class for the object which will reassociate functionality to the object.
     output(parsedObject.firstProperty);
     output(parsedObject['second Property']);
+
+    class MyClass {
+        // The constructor is a special method that is invoked when the class is instantiated.
+        constructor(fieldOne, propOne) {
+            this.exampleFieldOne = fieldOne;
+            // It's important to use the property mutator here, otherwise the value will not be validated.
+            this.exampleProperty = propOne;
+        }
+
+        // Fields ("dumb" variables)
+        exampleFieldOne;
+        exampleFieldTwo = 0;
+
+        // Property ("smart" variable)
+        // The # indicates that the backing field (in this case) is private - it cannot be directly accessed or mutated from outside the class.
+        #exampleProperty;
+        // In this accessor and mutator, no validation or formatting is being performed, they are serving as a simple pass-through to the backing field.
+        get exampleProperty() {
+            return this.#exampleProperty;
+        }
+        set exampleProperty(value) {
+            this.#exampleProperty = value;
+        }
+
+        // Methods are declared in a slightly different way than in an anonymous object.
+        firstFunc() {
+            output('firstFunc');
+        }
+
+        secondFunc() {
+            output('secondFunc');
+        }
+
+        thirdFunc = () => {
+            output('thirdFunc');
+        }
+
+        increment() {
+            this.exampleFieldTwo++;
+        }
+    }
+    output("---Class Result---");
+    const myObjectOfClass = new MyClass('exampleFieldOneValue', 'examplePropertyValue');
+    output(myObjectOfClass.exampleFieldOne);
+    output(myObjectOfClass.exampleFieldTwo);
+    output(myObjectOfClass.exampleProperty);
+    myObjectOfClass.firstFunc();
+    myObjectOfClass.secondFunc();
+    myObjectOfClass.thirdFunc();
+
+
+    const jsonClassObject = JSON.stringify(myObjectOfClass);
+    output(jsonClassObject);
+    const parseClassdObject = Object.assign(new MyClass(), JSON.parse(jsonClassObject));
+
+    output("---JSON Reimport Result---");
+    output(parseClassdObject.exampleFieldOne);
+    output(parseClassdObject.exampleFieldTwo);
+    output(parseClassdObject.exampleProperty);
+    parseClassdObject.firstFunc();
+    parseClassdObject.secondFunc();
+    parseClassdObject.thirdFunc();
+
+    parseClassdObject.increment();
+    parseClassdObject.increment();
+    parseClassdObject.increment();
+    output(parseClassdObject.exampleFieldTwo);
+    
 }
 
 
