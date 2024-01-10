bb38b4a9e3131e11ff2d0caed7d32ec1e4ed077e
James Grieve <jgrieve@ualberta.ca>
2024-01-09 19:36:09 -0700

    Method declarations in anonymous functions;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,27 @@
 async function main() {
-    // Properties can be defined either as 'variable names', or as strings.
+    function namedFunction() {
+        output('namedFunction');
+    }
+
     const myObject = {
+        // Properties can be defined either as 'variable names', or as strings.
         firstProperty: 'firstValue',
-        'second Property': 'secondValue'
+        'second Property': 'secondValue',
+        // Methods can be defined either as anonymous functions, or as references to named functions.
+        firstFunc: () => {
+            output('firstFunc');
+        },
+        secondFunc: namedFunction
     };
     
     // Either definition syntax can be accessed either with the dot operator or as an indexer, with the exception of if the key contains a space. In that case, you must use the indexer syntax.
     output(myObject.firstProperty);
     output(myObject['second Property']);
 
+    // Methods can also be invoked with either the dot operator or indexer syntax.
+    myObject.firstFunc();
+    namedFunction();
+    myObject['secondFunc']();
 }
 
 
