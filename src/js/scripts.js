5c4c00c48845d4831bb4acc0e4dc2ce4f7e4802c
James Grieve <jgrieve@ualberta.ca>
2024-01-19 19:42:46 -0700

    Instantiate directly into array;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,9 +4,11 @@
 async function main() {
   class Person {
     constructor(firstName, middleName, lastName) {
+      // If the class is directly instantiated, throw an error as it is abstract and should not be.
       if (this.constructor === Person) {
         throw new Error("This class is abstract and cannot be instantiated.");
       }
+      // By assigning these here, we reduce redundant code by avoiding repeating these lines in all 3 subclasses - it's okay to be here since the above error will prevent this class from being instantiated even though they are assigned.
       this.firstName = firstName;
       this.middleName = middleName;
       this.lastName = lastName;
@@ -16,10 +18,12 @@ async function main() {
     middleName;
     lastName;
 
+    // Bus Driver will get this from the parent class, but Student and Lawyer will override it.
     get fullName() {
       return `${this.firstName} ${this.middleName} ${this.lastName}`;
     }
 
+    // This is an abstract method that must be implemented in the child class. In JavaScript, it's really only here to tell the developer that they need to implement it in the child class - JavaScript has no way to enforce this otherwise.
     greet() {
       throw new Error("This function is abstract and must be implemented in the child class.");
     }
@@ -27,17 +31,22 @@ async function main() {
 
   class Student extends Person {
     constructor(firstName, middleName, lastName) {
+      // Call the parent constructor where the fields are assigned. No other actions need to be taken here.
       super(firstName, middleName, lastName);
     }
 
+    // Override the parent class's getter for fullName.
     get fullName() {
       return `${this.firstName} ${this.lastName}`;
     }
 
+    // Override the parent class's greet method.
     greet() {
       return `Hello, my name is ${this.fullName} and I am a student!`;
     }
   }
+
+  // The Lawyer and BusDriver classes are very similar, so I'm not going to comment them.
   class Lawyer extends Person {
     constructor(firstName, middleName, lastName) {
       super(firstName, middleName, lastName);
@@ -60,14 +69,15 @@ async function main() {
       return `Hello, my name is ${this.fullName} and I drive a bus!`;
     }
   }
+  // Create a polymorphic array of people. Since they all inherit from Person, we can assume they all have a greet method (if they don't, we'll get an error when we try to call it because we know it's abstract and must be implemented in the child class).
+  const people = [
+    new Student("John", "Jacob", "Jingleheimer-Schmidt"),
+    new Lawyer("Jane", "Jacob", "Jingleheimer-Schmidt"),
+    new BusDriver("Joe", "Jacob", "Jingleheimer-Schmidt"),
+    new BusDriver("Josh", "Jacob", "Jingleheimer-Schmidt"),
+  ];
 
-  const student = new Student("John", "Jacob", "Jingleheimer-Schmidt");
-  const lawyer = new Lawyer("Jane", "Jacob", "Jingleheimer-Schmidt");
-  const busDriver = new BusDriver("Joe", "Jacob", "Jingleheimer-Schmidt");
-  const anotherBusDriver = new BusDriver("Josh", "Jacob", "Jingleheimer-Schmidt");
-
-  const people = [student, lawyer, busDriver, anotherBusDriver];
-
+  // Loop through the array and call the greet method on each person. By using polymorphism, we don't need to know what type of person they are - we just know they have a greet method.
   for (const person of people) {
     output(person.greet());
   }
