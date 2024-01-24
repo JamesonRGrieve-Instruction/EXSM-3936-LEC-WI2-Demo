2590465554a15d129fde5222f5ce4f318239b4a3
James Grieve <jgrieve@ualberta.ca>
2024-01-23 19:42:42 -0700

    Use createElement to allow multiple images;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,5 +5,8 @@ const nameInput = document.querySelector("#input-name");
 const tagInput = document.querySelector("#input-tags");
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
-  main.innerHTML = `<img src="${urlInput.value}" alt="${nameInput.value}" />`;
+  const newImage = document.createElement("img");
+  newImage.src = urlInput.value;
+  newImage.alt = nameInput.value;
+  main.appendChild(newImage);
 });
