5bb67571b9600d954ae84be314b330c0f139eb00
James Grieve <jgrieve@ualberta.ca>
2024-01-23 19:46:47 -0700

    Add container div and heading to images;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,8 +5,14 @@ const nameInput = document.querySelector("#input-name");
 const tagInput = document.querySelector("#input-tags");
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
+  const newImageContainer = document.createElement("div");
+  newImageContainer.classList.add("image-container");
   const newImage = document.createElement("img");
   newImage.src = urlInput.value;
   newImage.alt = nameInput.value;
-  main.appendChild(newImage);
+  const newImageHeading = document.createElement("h2");
+  newImageHeading.textContent = nameInput.value;
+  newImageContainer.appendChild(newImageHeading);
+  newImageContainer.appendChild(newImage);
+  main.appendChild(newImageContainer);
 });
