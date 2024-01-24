5b89b17c935e8475a7dd2092e0c82a239732af52
James Grieve <jgrieve@ualberta.ca>
2024-01-23 20:16:53 -0700

    Add tag links;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,14 +5,38 @@ const nameInput = document.querySelector("#input-name");
 const tagInput = document.querySelector("#input-tags");
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
+
+  // Build Container
   const newImageContainer = document.createElement("div");
   newImageContainer.classList.add("image-container");
+  // Build Heading
+  const newImageHeading = document.createElement("h2");
+  newImageHeading.textContent = nameInput.value;
+  // Build Image
   const newImage = document.createElement("img");
   newImage.src = urlInput.value;
   newImage.alt = nameInput.value;
-  const newImageHeading = document.createElement("h2");
-  newImageHeading.textContent = nameInput.value;
+  // Build Tags
+  const newImageTags = document.createElement("ul");
+  // For each tag in the input, create a new li element.
+  for (const tag of tagInput.value.split(" ")) {
+    const newImageTag = document.createElement("li");
+
+    const newImageTagA = document.createElement("a");
+    // Prevent the link from trying to navigate.
+    newImageTagA.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+    newImageTagA.href = "#";
+    newImageTagA.textContent = "#" + tag;
+
+    newImageTag.appendChild(newImageTagA);
+    newImageTags.appendChild(newImageTag);
+  }
+
+  // Assemble New Element
   newImageContainer.appendChild(newImageHeading);
   newImageContainer.appendChild(newImage);
+  newImageContainer.appendChild(newImageTags);
   main.appendChild(newImageContainer);
 });
