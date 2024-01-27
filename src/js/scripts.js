76b538936139b32a67cb3e61d511e1e000d2aec2
James Grieve <jgrieve@ualberta.ca>
2024-01-26 19:23:54 -0700

    Add modal for image removal;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,14 +4,50 @@ const urlInput = document.querySelector("#input-url");
 const nameInput = document.querySelector("#input-name");
 const tagInput = document.querySelector("#input-tags");
 const searchInput = document.querySelector("#input-search");
+
+function clickRemove(event) {
+  // Create Modal
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.id = "remove-" + event.target.parentNode.parentNode.id;
+  // Create Confirm Button
+  const confirmButton = document.createElement("button");
+  confirmButton.textContent = "Confirm";
+  confirmButton.addEventListener("click", removeImage);
+  // Create Cancel Button
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Cancel";
+  removeButton.addEventListener("click", cancelModal);
+  // Build Modal
+  modal.appendChild(confirmButton);
+  modal.appendChild(removeButton);
+  main.appendChild(modal);
+}
+function removeImage(event) {
+  const target = document.querySelector("#" + event.target.parentNode.id.replace("remove-", ""));
+  target.remove();
+  event.target.parentNode.remove();
+}
+function cancelModal(event) {
+  event.target.parentNode.remove();
+}
+
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   // Build Container
   const newImageContainer = document.createElement("div");
+  newImageContainer.id = nameInput.value.replaceAll(" ", "-").toLowerCase();
   newImageContainer.classList.add("image-container");
+  const newImageContainerTitleBar = document.createElement("div");
+
   // Build Heading
   const newImageHeading = document.createElement("h2");
   newImageHeading.textContent = nameInput.value;
+  // Build Remove Button
+  const newImageRemoveButton = document.createElement("span");
+  newImageRemoveButton.textContent = "X";
+  newImageRemoveButton.addEventListener("click", clickRemove);
+
   // Build Image
   const newImage = document.createElement("img");
   newImage.src = urlInput.value;
@@ -34,11 +70,14 @@ submitButton.addEventListener("click", (event) => {
     newImageTags.appendChild(newImageTag);
   }
   // Assemble New Element
-  newImageContainer.appendChild(newImageHeading);
+  newImageContainerTitleBar.appendChild(newImageHeading);
+  newImageContainerTitleBar.appendChild(newImageRemoveButton);
+  newImageContainer.appendChild(newImageContainerTitleBar);
   newImageContainer.appendChild(newImage);
   newImageContainer.appendChild(newImageTags);
   main.appendChild(newImageContainer);
 });
+
 searchInput.addEventListener("input", (event) => {
   const search = event.target.value;
   const images = document.querySelectorAll(".image-container");
