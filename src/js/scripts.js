b1b5ecd04fe62b1d39f91f3c6ba001d6388f39e4
James Grieve <jgrieve@ualberta.ca>
2024-01-30 20:50:06 -0700

    Pieces on board;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,6 @@
+const blackClass = "fa-solid";
+const whiteClass = "fa-regular";
+const backRow = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"];
 const gameBoard = document.querySelector("main");
 function generateBoard(target) {
   for (let rowNumber = 1; rowNumber <= 8; rowNumber++) {
@@ -6,6 +9,16 @@ function generateBoard(target) {
     for (let colNumber = 97; colNumber <= 104; colNumber++) {
       const cell = document.createElement("div");
       cell.id = `${String.fromCharCode(colNumber)}${rowNumber}`;
+      if ([1, 2, 7, 8].includes(rowNumber)) {
+        const newPiece = document.createElement("i");
+        if (rowNumber === 1 || rowNumber === 8) {
+          newPiece.classList.add(`fa-chess-${backRow[colNumber - 97]}`);
+        } else if (rowNumber === 2 || rowNumber === 7) {
+          newPiece.classList.add(`fa-chess-pawn`);
+        }
+        newPiece.classList.add(rowNumber <= 4 ? blackClass : whiteClass);
+        cell.appendChild(newPiece);
+      }
       row.appendChild(cell);
     }
     target.appendChild(row);
