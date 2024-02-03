d8d37cd85d654ebe6d340bc8a01ab6a1eb85b13e
James Grieve <jgrieve@ualberta.ca>
2024-02-02 19:23:57 -0700

    Add piece movement;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@ const blackClass = "fa-solid";
 const whiteClass = "fa-regular";
 const backRow = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"];
 const gameBoard = document.querySelector("main");
+
 function generateBoard(target) {
   for (let rowNumber = 8; rowNumber >= 1; rowNumber--) {
     const row = document.createElement("div");
@@ -9,6 +10,7 @@ function generateBoard(target) {
     for (let colNumber = 97; colNumber <= 104; colNumber++) {
       const cell = document.createElement("div");
       cell.id = `${String.fromCharCode(colNumber)}${rowNumber}`;
+      cell.addEventListener("click", movePiece);
       if ([1, 2, 7, 8].includes(rowNumber)) {
         const newPiece = document.createElement("i");
         if (rowNumber === 1 || rowNumber === 8) {
@@ -17,6 +19,7 @@ function generateBoard(target) {
           newPiece.classList.add(`fa-chess-pawn`);
         }
         newPiece.classList.add(rowNumber >= 4 ? blackClass : whiteClass);
+        newPiece.addEventListener("click", selectPiece);
         cell.appendChild(newPiece);
       }
       row.appendChild(cell);
@@ -25,3 +28,24 @@ function generateBoard(target) {
   }
 }
 generateBoard(gameBoard);
+
+function selectPiece(event) {
+  const targetPiece = event.target;
+  if (targetPiece.tagName === "I") {
+    targetPiece.classList.add("selected");
+    //const oldCell = piece.parentElement;
+    //const oldRow = oldCell.parentElement;
+    //const oldCol = oldRow.id;
+    //const newRow = gameBoard.querySelector("#row-5");
+    //const newCell = newRow.querySelector("#d5");
+    //newCell.appendChild(piece);
+  }
+}
+function movePiece(event) {
+  const targetSpace = event.target;
+  const selectedPiece = gameBoard.querySelector(".selected");
+  if (selectedPiece) {
+    targetSpace.appendChild(selectedPiece);
+    selectedPiece.classList.remove("selected");
+  }
+}
