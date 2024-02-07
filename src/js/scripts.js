912c857e5306360a48cb2fecdf40bab9db139695
James Grieve <jgrieve@ualberta.ca>
2024-02-06 20:33:32 -0700

    Async console;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
-async function main() {}
+async function main() {
+  const tasks = [example("One", 1000), example("Two", 5000), example("Three", 2500)];
+  await Promise.all(tasks);
+  output("Done");
+}
+async function example(text, timeout) {
+  await sleep(timeout);
+  output("Hello World! " + text);
+}
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
