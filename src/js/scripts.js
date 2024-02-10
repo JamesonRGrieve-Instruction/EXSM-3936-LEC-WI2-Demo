131cb3e870ed49ae394c1b4f7e1873053d53c555
James Grieve <jgrieve@ualberta.ca>
2024-02-09 19:51:39 -0700

    Basic user rendering;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,65 +1,55 @@
-const jokeButton = document.querySelector("#getJoke");
+const userButton = document.querySelector("#loadUser");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
-const displayedJokeIDs = [];
+let userPage = 1;
+let loading = false;
 
-async function getJoke() {
-  let joke;
-  try {
-    joke = await axios.get("https://v2.jokeapi.dev/joke/Programming?safe-mode");
-    if (joke.status !== 200) {
-      throw new Error(`Failed to fetch joke with status ${joke.status}: ${joke.statusText}.`);
-    }
-  } catch {
-    throw new Error(`Failed to fetch joke.`);
+async function getUser() {
+  const user = await fetch("https://randomuser.me/api/?seed=exsm3936&inc=name,email,phone,picture&page=" + userPage);
+  if (user.status !== 200) {
+    throw new Error(`Failed to fetch user with status ${user.status}: ${user.statusText}.`);
   }
-  return joke.data;
+  return (await user.json()).results[0];
 }
-jokeButton.addEventListener("click", async () => {
-  let joke;
-  try {
-    joke = await getJoke();
-    while (displayedJokeIDs.includes(joke.id)) {
-      // For testing purposes - can spam click the button and watch for an error.
-      //console.error(`Duplicate joke found with ID ${joke.id}, fetching another.`);
-      joke = await getJoke();
+userButton.addEventListener("click", async () => {
+  if (!loading) {
+    let user;
+    try {
+      loading = true;
+      user = await getUser();
+      userPage++;
+    } catch (error) {
+      console.error(error);
+      let errorElement = document.querySelector("footer p");
+      if (errorElement === null) {
+        errorElement = document.createElement("p");
+        footer.appendChild(errorElement);
+      }
+      errorElement.textContent = "ERROR: " + error.message;
     }
-    displayedJokeIDs.push(joke.id);
-  } catch (error) {
-    console.error(error);
-    let errorElement = document.querySelector("footer p");
-    if (errorElement === null) {
-      errorElement = document.createElement("p");
-      footer.appendChild(errorElement);
-    }
-    errorElement.textContent = "ERROR: " + error.message;
-  }
+    if (user) {
+      const userElement = document.createElement("div");
 
-  if (joke.type === "single") {
-    const jokeElement = document.createElement("p");
-    jokeElement.textContent = joke.joke;
-    main.appendChild(jokeElement);
-  } else if (joke.type === "twopart") {
-    // Create the setup.
-    const setupElement = document.createElement("p");
-    setupElement.textContent = joke.setup;
-    // Create the punchline.
-    const deliveryElement = document.createElement("p");
-    deliveryElement.textContent = joke.delivery;
-    // Start the punchline as hidden.
-    deliveryElement.classList.add("hidden");
-    // Create the reveal button.
-    const revealButton = document.createElement("button");
-    revealButton.textContent = "Reveal Punchline";
-    // Add the event listener to reveal the punchline.
-    revealButton.addEventListener("click", () => {
-      deliveryElement.classList.remove("hidden");
-      revealButton.remove();
-    });
-    // Append the elements to the main element.
-    main.appendChild(setupElement);
-    main.appendChild(revealButton);
-    main.appendChild(deliveryElement);
+      const userName = document.createElement("h2");
+      userName.textContent = `${user.name.first} ${user.name.last}`;
+      userElement.appendChild(userName);
+
+      const userImage = document.createElement("img");
+      userImage.src = user.picture.large;
+      userElement.appendChild(userImage);
+
+      const userEmail = document.createElement("a");
+      userEmail.href = `mailto:${user.email}`;
+      userEmail.textContent = user.email;
+      userElement.appendChild(userEmail);
+
+      const userPhone = document.createElement("a");
+      userPhone.href = `tel:${user.phone}`;
+      userPhone.textContent = user.phone;
+      userElement.appendChild(userPhone);
+
+      main.appendChild(userElement);
+    }
+    loading = false;
   }
-  main.appendChild(document.createElement("hr"));
 });
