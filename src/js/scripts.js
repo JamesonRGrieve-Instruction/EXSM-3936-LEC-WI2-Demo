bce6819202430be43f34d489a23e05e4aaa5cc6e
James Grieve <jgrieve@ualberta.ca>
2024-02-09 20:45:34 -0700

    Implement QR resizing;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,13 +5,26 @@ let activeQR = null;
 urlInput.addEventListener("input", async (event) => {
   if (event.target.value) {
     const qrCode = await (
-      await fetch(`http://api.qrserver.com/v1/create-qr-code/?data=${event.target.value}&size=100x100`)
+      await fetch(
+        `http://api.qrserver.com/v1/create-qr-code/?data=${event.target.value}&size=${
+          placeholder.classList.contains("image-large") ? "500x500" : "100x100"
+        }`,
+      )
     ).blob();
     if (activeQR) {
       activeQR.remove();
       placeholder.classList.remove("hidden");
     }
     activeQR = document.createElement("img");
+    if (placeholder.classList.contains("image-large")) {
+      activeQR.classList.add("image-large");
+    }
+    activeQR.addEventListener("click", () => {
+      activeQR.classList.toggle("image-large");
+      placeholder.classList.toggle("image-large");
+      // Re-fire the input change event if we click, which will re-render the QR code in the updated size.
+      urlInput.dispatchEvent(event);
+    });
     activeQR.src = URL.createObjectURL(qrCode);
     qrTarget.appendChild(activeQR);
     placeholder.classList.add("hidden");
